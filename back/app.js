7867406716f7556261b7c2b1fce5a9025e64a801
sabanyilmaz1 <syilmaz1>
2022-11-19 19:47:19 +0100

    setting up of project files

--- back/app.js
+++ back/app.js
@@ -1,5 +1,34 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+const connect = require("./connection");
+
 const app = express();
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use((req, res) => {
+  res.json({ message: "Votre requete a bien été reçue" });
+});
+
+mongoose
+  .connect(connect().toString(), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 module.exports = app;
---
+++ back/connection.js
@@ -0,0 +1,7 @@
+//Permet de faire le lien avec ma base de donnée mongodb
+
+function connect() {
+  return "mongodb+srv://saban:SWdGHFTwmMXmWgjQ@cluster0.drbz3d8.mongodb.net/?retryWrites=true&w=majority";
+}
+
+module.exports = connect;
